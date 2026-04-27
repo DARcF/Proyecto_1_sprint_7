a810cbbbc940f37946365f5f66ddae64504531de
Diego Reyes <diegoreyesc2@gmail.com>
2026-04-27 17:18:12 -0600

    Entrega1.1

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ import plotly.graph_objects as go
 
 # Configuración básica de la app
 st.header("Análisis de anuncios de venta de vehículos en EE. UU.")
-
+st.write("Essta Aplicación muestra un DAsboard dinámico con python")
 
 # Cargar Datos
 ruta = r"C:\Users\diego\OneDrive\Desktop\Triple_Ten\intro_ml\Proyecto_1_sprint_7\vehicles_us.csv"
@@ -45,27 +45,3 @@ if scatter_button:
         yaxis_title="Precio"
     )
     st.plotly_chart(fig_scatter, use_container_width=True)
-"""       
-# Lógica a ejecutar cuando se hace clic en el botón
-if hist_button:
-    # Escribir un mensaje en la aplicación
-    st.write(
-        'Creación de un histograma para el conjunto de datos de anuncios de venta de coches')
-
-    # Crear un histograma utilizando plotly.graph_objects
-    # Se crea una figura vacía y luego se añade un rastro de histograma
-    fig = go.Figure(data=[go.Histogram(x=car_data['odometer'])])
-
-    # Opcional: Puedes añadir un título al gráfico si lo deseas
-    fig.update_layout(title_text='Distribución del Odómetro')
-
-    # Mostrar el gráfico Plotly interactivo en la aplicación Streamlit
-    # 'use_container_width=True' ajusta el ancho del gráfico al contenedor
-    st.plotly_chart(fig, use_container_width=True)
-
-    build_histogram = st.checkbox('Construir un histograma')
-
-    if build_histogram:  # si la casilla de verificación está seleccionada
-        st.write('Construir un histograma para la columna odómetro')
-        ...
-        """
